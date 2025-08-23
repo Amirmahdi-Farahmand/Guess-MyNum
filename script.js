33e5eb199fb7237d95a560a507b2bc6470bc82e0
Amirmahdi Farahmand <amirmahdifarahmandrasekh@gmail.com>
2025-08-23 11:23:27 +0330

    Update script.js

--- script.js
+++ script.js
@@ -78,6 +78,6 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
 
   // Reset styles to default
-  document.querySelector('body').style.backgroundColor = '#eee';
+  document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
